20ea44728814834b6bf8b8f55079b454c44763b9
Sergey Yelin <elinsn@gmail.com>
2010-12-13 15:17:23 +0300

    New handlers for everu REST operation has been added

--- src/handlers/gs_request_handler.erl
+++ src/handlers/gs_request_handler.erl
@@ -2,12 +2,29 @@
 -author('Sergey Yelin <elinsn@gmail.com>').
 -vsn("1.0.0").
 
-% API
+% Public API
 -export([handle_request/1]).
+
+%Intenral API
+-export([handle/2]).
 
 %% callback on request received
 handle_request(Req) ->
-    Req:ok(<<"Hello, World!">>).
+    ?MODULE:handle(Req:get(method), Req).   
+%    Req:ok(<<"Hello, World!">>).
 
 % ---------------------------- /\ handle rest --------------------------------------------------------------
 
+% Handle creates
+handle('PUT', Req) ->
+    gs_create_chain_handler:handle_request(Req);
+% Handle reads
+handle('GET', Req) ->
+    gs_read_chain_handler:handle_request(Req);
+% Handle updates
+handle('POST', Req) ->
+    gs_create_update_handler:handle_request(Req);
+% Handle deletes
+handle('DELETE', Req) ->
+    gs_delete_chain_handler:handle_request(Req).
+    
