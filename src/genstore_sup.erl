968e43c92bedae216d99582a5fb3e1f6688e318a
Sergey Yelin <elinsn@gmail.com>
2010-12-15 10:32:05 +0300

    Run misultin from superviser directly

--- src/genstore_sup.erl
+++ src/genstore_sup.erl
@@ -10,7 +10,7 @@
 -export([init/1]).
 
 %% Helper macro for declaring children of supervisor
--define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
+-define(CHILD(I, Options, Type), {I, {I, start_link, [Options]}, permanent, 5000, Type, [I]}).
 
 %% ===================================================================
 %% API functions
@@ -24,6 +24,12 @@ start_link() ->
 %% ===================================================================
 
 init([]) ->
-    Http = ?CHILD(genstore_http, worker),
+    {ok, MisultinConfig} = misultin_config(),
+    Http = ?CHILD(misultin, MisultinConfig, worker),
     {ok, { {one_for_one, 5, 10}, [Http]} }.
 
+misultin_config() ->
+    {ok, Config} = application:get_env(misultin),
+    {value, {handler, Module, Function}} = lists:keysearch(handler, 1, Config), 
+    Config1 = Config ++ [{loop, fun(Req) -> erlang:apply(Module, Function, [Req]) end}],
+    {ok, Config1}.
